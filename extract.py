3d732d4fdcd048847f033bc9f4bddb1270509ed1
SunDoge <384813529@qq.com>
2018-12-28 16:49:01 +0800

    format extract.py and remove variable, use no_grad instead

--- extract.py
+++ extract.py
@@ -8,10 +8,9 @@ import torch
 import torch.nn as nn
 import torch.nn.parallel
 import torch.backends.cudnn as cudnn
-from torch.autograd import Variable
 
 import torchvision.transforms as transforms
-import torchvision.datasets as datasets
+# import torchvision.datasets as datasets
 
 import vqa.models.convnets as convnets
 import vqa.datasets as datasets
@@ -29,8 +28,8 @@ parser.add_argument('--data_split', default='train', type=str,
 parser.add_argument('--arch', '-a', default='fbresnet152',
                     choices=convnets.model_names,
                     help='model architecture: ' +
-                        ' | '.join(convnets.model_names) +
-                        ' (default: fbresnet152)')
+                    ' | '.join(convnets.model_names) +
+                    ' (default: fbresnet152)')
 parser.add_argument('--workers', default=4, type=int,
                     help='number of data loading workers (default: 4)')
 parser.add_argument('--batch_size', '-b', default=80, type=int,
@@ -46,7 +45,8 @@ def main():
     args = parser.parse_args()
 
     print("=> using pre-trained model '{}'".format(args.arch))
-    model = convnets.factory({'arch':args.arch}, cuda=True, data_parallel=True)
+    model = convnets.factory(
+        {'arch': args.arch}, cuda=True, data_parallel=True)
 
     extract_name = 'arch,{}_size,{}'.format(args.arch, args.size)
 
@@ -57,28 +57,29 @@ def main():
         if 'coco' not in args.dir_data:
             raise ValueError('"coco" string not in dir_data')
         dataset = datasets.COCOImages(args.data_split, dict(dir=args.dir_data),
-            transform=transforms.Compose([
-                transforms.Scale(args.size),
-                transforms.CenterCrop(args.size),
-                transforms.ToTensor(),
-                normalize,
-            ]))
+                                      transform=transforms.Compose([
+                                          transforms.Scale(args.size),
+                                          transforms.CenterCrop(args.size),
+                                          transforms.ToTensor(),
+                                          normalize,
+                                      ]))
     elif args.dataset == 'vgenome':
         if args.data_split != 'train':
             raise ValueError('train split is required for vgenome')
         if 'vgenome' not in args.dir_data:
             raise ValueError('"vgenome" string not in dir_data')
         dataset = datasets.VisualGenomeImages(args.data_split, dict(dir=args.dir_data),
-            transform=transforms.Compose([
-                transforms.Scale(args.size),
-                transforms.CenterCrop(args.size),
-                transforms.ToTensor(),
-                normalize,
-            ]))
+                                              transform=transforms.Compose([
+                                                  transforms.Scale(args.size),
+                                                  transforms.CenterCrop(
+                                                      args.size),
+                                                  transforms.ToTensor(),
+                                                  normalize,
+                                              ]))
 
     data_loader = DataLoader(dataset,
-        batch_size=args.batch_size, shuffle=False,
-        num_workers=args.workers, pin_memory=True)
+                             batch_size=args.batch_size, shuffle=False,
+                             num_workers=args.workers, pin_memory=True)
 
     dir_extract = os.path.join(args.dir_data, 'extract', extract_name)
     path_file = os.path.join(dir_extract, args.data_split + 'set')
@@ -93,54 +94,56 @@ def extract(data_loader, model, path_file, mode):
     hdf5_file = h5py.File(path_hdf5, 'w')
 
     # estimate output shapes
-    output = model(Variable(torch.ones(1, 3, args.size, args.size),
-                            volatile=True))
+    with torch.no_grad():
+        output = model(torch.ones(1, 3, args.size, args.size))
 
     nb_images = len(data_loader.dataset)
     if mode == 'both' or mode == 'att':
         shape_att = (nb_images, output.size(1), output.size(2), output.size(3))
         print('Warning: shape_att={}'.format(shape_att))
         hdf5_att = hdf5_file.create_dataset('att', shape_att,
-                                            dtype='f')#, compression='gzip')
+                                            dtype='f')  # , compression='gzip')
     if mode == 'both' or mode == 'noatt':
         shape_noatt = (nb_images, output.size(1))
         print('Warning: shape_noatt={}'.format(shape_noatt))
         hdf5_noatt = hdf5_file.create_dataset('noatt', shape_noatt,
-                                              dtype='f')#, compression='gzip')
+                                              dtype='f')  # , compression='gzip')
 
     model.eval()
 
     batch_time = AvgMeter()
-    data_time  = AvgMeter()
+    data_time = AvgMeter()
     begin = time.time()
     end = time.time()
 
     idx = 0
-    for i, input in enumerate(data_loader):
-        input_var = Variable(input['visual'], volatile=True)
-        output_att = model(input_var)
+    with torch.no_grad():
+        for i, input in enumerate(data_loader):
+            input_var = input['visual']
+            output_att = model(input_var)
 
-        nb_regions = output_att.size(2) * output_att.size(3)
-        output_noatt = output_att.sum(3).sum(2).div(nb_regions).view(-1, 2048)
+            nb_regions = output_att.size(2) * output_att.size(3)
+            output_noatt = output_att.sum(3).sum(
+                2).div(nb_regions).view(-1, 2048)
 
-        batch_size = output_att.size(0)
-        if mode == 'both' or mode == 'att':
-            hdf5_att[idx:idx+batch_size]   = output_att.data.cpu().numpy()
-        if mode == 'both' or mode == 'noatt':
-            hdf5_noatt[idx:idx+batch_size] = output_noatt.data.cpu().numpy()
-        idx += batch_size
+            batch_size = output_att.size(0)
+            if mode == 'both' or mode == 'att':
+                hdf5_att[idx:idx+batch_size] = output_att.data.cpu().numpy()
+            if mode == 'both' or mode == 'noatt':
+                hdf5_noatt[idx:idx+batch_size] = output_noatt.data.cpu().numpy()
+            idx += batch_size
 
-        torch.cuda.synchronize()
-        batch_time.update(time.time() - end)
-        end = time.time()
+            torch.cuda.synchronize()
+            batch_time.update(time.time() - end)
+            end = time.time()
 
-        if i % 1 == 0:
-            print('Extract: [{0}/{1}]\t'
-                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'.format(
-                   i, len(data_loader),
-                   batch_time=batch_time,
-                   data_time=data_time,))
+            if i % 1 == 0:
+                print('Extract: [{0}/{1}]\t'
+                      'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                      'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'.format(
+                          i, len(data_loader),
+                          batch_time=batch_time,
+                          data_time=data_time,))
 
     hdf5_file.close()
 
@@ -150,7 +153,7 @@ def extract(data_loader, model, path_file, mode):
             handle.write(name + '\n')
 
     end = time.time() - begin
-    print('Finished in {}m and {}s'.format(int(end/60), int(end%60)))
+    print('Finished in {}m and {}s'.format(int(end/60), int(end % 60)))
 
 
 if __name__ == '__main__':
